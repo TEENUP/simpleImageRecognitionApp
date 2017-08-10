d79f64355e92e3e4a54f0fa75758dfb95737d39e
Yashi Pallav <puneetgupta5294@gmail.com>
2017-08-10 12:13:29 +0530

    UI Completed and added buttons for opening camera and photolibrary

--- simpleImageRecognitionApp/simpleImageRecognitionApp/ViewController.swift
+++ simpleImageRecognitionApp/simpleImageRecognitionApp/ViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import Ma
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UINavigationControllerDelegate {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var classifierLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func camera(_ sender: Any) {
+        
+        if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+            return
+        }
+        
+        let cameraPicker = UIImagePickerController()
+        cameraPicker.sourceType = .camera
+        cameraPicker.allowsEditing = false
+        cameraPicker.delegate = self
+        
+        present(cameraPicker, animated: true)
+    }
+    
+    @IBAction func openLibrary(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = false
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true )
+    }
 
 }
 
+extension ViewController: UIImagePickerControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+}
